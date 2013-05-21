b5ce10dacc10b81fa4752b02a8ae9fff692f4610
Ben Ernst <boinst@gmail.com>
2013-05-21 11:10:52 +0930

    (+) Added function int::to-hexadecimal (+) Added function win32::format-win32-error

---
+++ Boinst.NAntExtensions/Functions/IntFunctions.cs
@@ -0,0 +1,37 @@
+﻿namespace Boinst.NAntExtensions
+{
+    using NAnt.Core;
+    using NAnt.Core.Attributes;
+
+    /// <summary>
+    /// Functions that operate on integers
+    /// </summary>
+    [FunctionSet("int", "Int")]
+    public class IntFunctions : FunctionSetBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntFunctions"/> class.
+        /// </summary>
+        public IntFunctions(Project project, PropertyDictionary propDict)
+            : base(project, propDict)
+        {
+        }
+        
+        /// <summary>
+        /// Returns the given integer formated as hexadecimal
+        /// </summary>
+        /// <param name="val">
+        /// The integer value to format as hexadecimal.
+        /// </param>
+        /// <example>
+        /// <code>
+        /// ${int::to-hexadecimal(-1073741515)} ==&gt; C0000135
+        /// </code>
+        /// </example>
+        [Function("to-hexadecimal")]
+        public static string ToHexadecimal(int val)
+        {
+            return val.ToString("X");
+        }
+    }
+}
---
+++ Boinst.NAntExtensions/Functions/Win32Functions.cs
@@ -0,0 +1,44 @@
+﻿namespace Boinst.NAntExtensions
+{
+    using NAnt.Core;
+    using NAnt.Core.Attributes;
+
+    /// <summary>
+    /// Functions that operate on integers
+    /// </summary>
+    [FunctionSet("win32", "Win32")]
+    public class Win32Functions : FunctionSetBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Win32Functions"/> class.
+        /// </summary>
+        public Win32Functions(Project project, PropertyDictionary propDict)
+            : base(project, propDict)
+        {
+        }
+
+        /// <summary>
+        /// Format a Win32 error code.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// ${win32::format-win32-error(0)} ==&gt; 0 (0): No Error
+        /// </code>
+        /// </example>
+        [Function("format-win32-error")]
+        public static string FormatWin32Error(int result)
+        {
+            string msg = string.Format("{0} ({1})", result, result.ToString("X"));
+            string errorDetail = result == 0 ? "No Error" : GetLastWin32Error();
+            if (!string.IsNullOrWhiteSpace(errorDetail)) msg += ": " + errorDetail;
+            return msg;
+        }
+
+        [Function("get-last-win32-error")]
+        public static string GetLastWin32Error()
+        {
+            string errmsg = new System.ComponentModel.Win32Exception(System.Runtime.InteropServices.Marshal.GetLastWin32Error()).Message;
+            return string.IsNullOrWhiteSpace(errmsg) ? string.Empty : errmsg;
+        }
+    }
+}
